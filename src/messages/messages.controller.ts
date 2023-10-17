bb890b06338c1a30f78d402de3788912ec358dab
Haren-Sharma <harensharma33@gmail.com>
2023-10-17 11:09:46 +0530

    reporting errors with exceptions

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,23 +1,33 @@
-import { Controller,Get,Post,Body,Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { creteMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-    messagesservice:MessagesService
-    constructor(){
-        this.messagesservice=new MessagesService() //don't do this way,make use of dependency injection instead
-    } 
-    @Get()
-    listMessgaes(){
-        return this.messagesservice.findAll();
-    }
-    @Post()
-    createMessage(@Body() body:creteMessageDto){
-      return  this.messagesservice.create(body.content)
-    }
-    @Get('/:id')
-    getMessage(@Param('id') id:string){
-        return this.messagesservice.findOne(id);
-    }
+  messagesservice: MessagesService;
+  constructor() {
+    //don't do this way,make use of dependency injection instead
+    this.messagesservice = new MessagesService();
+  }
+  @Get()
+  listMessgaes() {
+    return this.messagesservice.findAll();
+  }
+  @Post()
+  createMessage(@Body() body: creteMessageDto) {
+    return this.messagesservice.create(body.content);
+  }
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const msg = await this.messagesservice.findOne(id);
+    if (!msg) throw new NotFoundException('Message Not Found');
+    return msg;
+  }
 }
