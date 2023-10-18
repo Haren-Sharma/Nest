11d655aeef6ede3509a105bacf34f8ba687f7d23
Haren-Sharma <harensharma33@gmail.com>
2023-10-18 11:01:51 +0530

    global pipes validation

--- mycar/src/main.ts
+++ mycar/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true //validator will strip validated (returned) object of any properties that do not use any validation decorators.
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
